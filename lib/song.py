3e4e45e15efa8df822ac0c2d576d92dc026b53d4
Brian Lawi Ochiel <lawioochiel@gmail.com>
2024-01-30 17:00:32 +0300

    completed task

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,22 @@
 class Song:
-    pass
+    count = 0
+    artists = []
+    genres = []
+    genre_count = {}
+    artist_count= {}
+    
+    def __init__(self,name,artist,genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        Song.count += 1
+        Song.artists.append(self.artist)
+        Song.genres.append(self.genre)
+        if self.genre in Song.genre_count:
+            Song.genre_count[self.genre] += 1
+        else:
+            Song.genre_count[self.genre] = 1
+        if self.artist in Song.artist_count:
+            Song.artist_count[self.artist] += 1
+        else:
+            Song.artist_count[self.artist] = 1
